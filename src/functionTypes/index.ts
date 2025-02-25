6bef0813392edbb5acbf528fa171734a52e107d1
morganharris39 <ravenstar03@gmail.com>
2025-02-24 17:44:47 -0700

    caught up to class

--- src/functionTypes/index.ts
+++ src/functionTypes/index.ts
@@ -1,47 +1,73 @@
 import {
-    functionParams,
-    functionReturnTypesOne,
-    functionReturnTypesTwo,
-  } from '../examples/functionTypes/examples'
-  import { loadExamples } from '../utils/exampleLoadFuncs'
-  
-  const examples = [functionParams(), functionReturnTypesOne(), functionReturnTypesTwo()]
-  
-  let exampleCounter = 0
-  
-  const leftNavButton = document.querySelector('#leftNavButton')
-  const rightNavButton = document.querySelector('#rightNavButton')
-  
-  if (leftNavButton) {
-    // figure out if the example counter is greater than 0
-    leftNavButton.addEventListener('click', () => {
-      if (exampleCounter > 0) {
-        exampleCounter--
-      } 
-      console.log('exampleCounter value is: ', exampleCounter)
-      loadExamples([examples[exampleCounter]])
+  functionParams,
+  functionReturnTypesOne,
+  functionReturnTypesTwo,
+} from '../examples/functionTypes/examples'
+import { loadExamples, loadExample } from '../utils/exampleLoadFuncs'
+import type { CodeExample } from '../types/CodeExample'
+
+const examples: CodeExample[] = [
+  functionParams(),
+  functionReturnTypesOne(),
+  functionReturnTypesTwo(),
+]
+
+let exampleCounter = 0
+
+const leftNavButton = document.querySelector('#leftNavButton')
+const rightNavButton = document.querySelector('#rightNavButton')
+
+if (leftNavButton) {
+  if (exampleCounter === 0) {
+    leftNavButton.classList.toggle('hidden')
+  }
+
+  // figure out if the example counter is greater than 0
+  leftNavButton.addEventListener('click', () => {
+    if (exampleCounter > 0) {
+      exampleCounter--
+    } // else if exampleCounter is equal to 0, hide the leftNavButton
+    // if (exampleCounter === 0) {
+    //   leftNavButton.classList.add('hidden')
+    // }
+    console.log(
+      'Left Nav exampleCounter value is decremented to: ',
+      exampleCounter
+    )
+    loadExample(examples[exampleCounter])
   })
-  }
-  
-  if (rightNavButton) {
-    const examples = [functionReturnTypesOne()]
-    rightNavButton.addEventListener('click', () => loadExamples(examples))
-  }
-  
-  function getName(): string {
-    return 'John Doe'
-  }
-  
-  const getNameArrow = (): string => 'John Doe'
-  
-  function calculateArea(radius: number): number {
-    return Math.PI * radius * radius
-  }
-  
-  const calculateAreaArrow = (radius: number): number => Math.PI * radius * radius
-  
-  function logMessage(msg: string): void {
-    console.log(msg)
-  }
-  
-  const logMessageArrow = (msg: string): void => console.log(msg)
+}
+
+if (rightNavButton) {
+  if (exampleCounter === examples.length - 1) {
+    rightNavButton.classList.add('hidden')
+  } 
+
+  rightNavButton.addEventListener('click', () => {
+    if (exampleCounter < examples.length - 1) {
+      exampleCounter++
+    } // else if exampleCounter is equal to the length of the examples array - 1, hide the rightNavButton
+
+    loadExample(examples[exampleCounter])
+  })
+}
+
+loadExample(examples[exampleCounter])
+
+/* function getName(): string {
+  return 'John Doe'
+}
+
+const getNameArrow = (): string => 'John Doe'
+
+function calculateArea(radius: number): number {
+  return Math.PI * radius * radius
+}
+
+const calculateAreaArrow = (radius: number): number => Math.PI * radius * radius
+
+function logMessage(msg: string): void {
+  console.log(msg)
+}
+
+const logMessageArrow = (msg: string): void => console.log(msg) */
